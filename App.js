285ea2549c5da976694873cf237c097a983bccc1
antoniovictor2k <avps2@aluno.ifal.edu.br>
2023-07-11 14:13:16 -0300

    att teste

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import {React, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import TelaMenu from './src/pages/TelaMenu';
 
+// AntonioVictor
+
 const App = () => {
 
   return (
